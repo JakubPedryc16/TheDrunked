08ab224732aae40ad6b2dc9639fc09fba0ee91e3
JakubPedryc16 <jakubpedryc@gmail.com>
2024-08-14 16:36:59 +0200

    Authorization and connecting backend with frontend, cors service added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,33 @@ import Login from "./pages/Login.tsx"
 import Register from "./pages/Register.tsx"
 import Home from "./pages/Home.tsx"
 import NotFound from "./pages/NotFound.tsx"
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+
+function Logout() {
+    localStorage.clear();
+    return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+    localStorage.clear();
+    return <Register />;
+}
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route
+                    path="/"
+                    element={
+                    <ProtectedRoute>
+                        <Home/>
+                    </ProtectedRoute>
+                }/>
+
+                <Route path="/logout" element={<Logout />} />
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
+                <Route path="/register" element={<RegisterAndLogout/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
--- src/main/java/com/pedryc/thedrunked/TheDrunkedApplication.java
+++ src/main/java/com/pedryc/thedrunked/TheDrunkedApplication.java
@@ -2,6 +2,7 @@ package com.pedryc.thedrunked;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class TheDrunkedApplication {
